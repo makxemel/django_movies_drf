492eb9fe81e1c7aa1e017ea4ce4e6d0ca96f7589
makx <makxemelianov@gmail.com>
2023-03-19 15:04:48 +0300

    add project

---
+++ movies/views.py
@@ -0,0 +1,69 @@
+from django.db import models
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Movie, Actor
+from .serializers import (
+    MovieListSerializer,
+    MovieDetailSerializer,
+    ReviewCreateSerializer,
+    CreateRatingSerializer,
+    ActorListSerializer,
+    ActorDetailSerializer,
+)
+from .service import get_client_ip
+
+
+class MovieListView(APIView):
+    """"Вывод списка фильмов"""
+    def get(self, request):
+        movies = Movie.objects.filter(draft=False).annotate(
+            # рейтинг фильма от пользователя
+            rating_user=models.Count('ratings', filter=models.Q(ratings__ip=get_client_ip(request)))
+        ).annotate(
+            # средний рейтинг фильма
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data)
+
+
+class MovieDetailView(APIView):
+    """"Вывод фильма"""
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk, draft=False)
+        serializer = MovieDetailSerializer(movie, many=False)
+        return Response(serializer.data)
+
+
+class ReviewCreateView(APIView):
+    """Добавление отзыва к фильму"""
+    def post(self, request):
+        review = ReviewCreateSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+        return Response(status=201)
+
+
+class AddStarRatingView(APIView):
+    """Добавление рейтинга к фильму"""
+    def post(self, request):
+        serializer = CreateRatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(ip=get_client_ip(request))
+            return Response(status=201)
+        else:
+            return Response(status=400)
+
+
+class ActorsListView(generics.ListAPIView):
+    """Вывод списка актеров"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorListSerializer
+
+
+class ActorDetailView(generics.RetrieveAPIView):
+    """Вывод актера или режиссера"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorDetailSerializer
